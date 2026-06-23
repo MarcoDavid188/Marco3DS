b6930046d1f48800e66b1ee6fe1f758b60b235de
Marco David Amaya Portillo <5613917@clases.edu.sv>
2026-06-23 08:16:48 -0600

    Añadidas consultas estadísticas y agrupamientos para el Dashboard

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -73,3 +73,26 @@ SELECT p.nombre_producto,
 FROM productos p
 INNER JOIN categorias c
 ON p.categoria_id = c.id;
+-- ====================================================================
+-- CONSULTAS DE ESTADÍSTICAS Y MÉTRICAS PARA EL DASHBOARD (Guía 12)
+-- ====================================================================
+
+-- Tarjeta 1
+SELECT COUNT(id) AS total_productos_catalogo
+FROM productos;
+
+-- Tarjeta 2
+SELECT SUM(precio * stock) AS valor_monetario_inventario
+FROM productos;
+
+-- Tarjeta 3
+SELECT MAX(precio) AS producto_mas_caro
+FROM productos;
+
+-- Tarjeta 4
+SELECT c.nombre_categoria,
+SUM(p.stock) AS existencias_totales
+FROM productos p
+INNER JOIN categorias c
+ON p.categoria_id = c.id
+GROUP BY c.nombre_categoria;
